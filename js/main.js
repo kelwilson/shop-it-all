2aee3f7d872c40ebbbdc954b0e1ea34b096f07f6
kelwilson <kelwilsonachienu@gmail.com>
2024-07-05 01:14:08 +0800

    Fix bug removed category duplicates

--- js/main.js
+++ js/main.js
@@ -1,39 +1,30 @@
-//Example fetch using pokemonapi.co
-// document.querySelector('button').addEventListener('click', getFetch)
-
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-//   console.log(choice)
-//   const url = `https://api.nasa.gov/planetary/apod?api_key=iJy0Nd4wZZzfW2HFgBkNV4DDUsZ6PxL3RqQRgFfI&date=${choice}`
-// }
 
 const url = `https://fakestoreapi.com/products`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-          console.log(data)
 
  // Getting the Categories         
-          const categories = []
-          data.forEach(obj => { 
-              categories.push(obj.category)
-            })
-            const setCategory = [...new Set(categories)]
-            console.log(setCategory);
-            const listItem = document.createElement('li')
-            setCategory.forEach(ele => {
-                console.log(ele);
-                listItem.innerHTML = `
-                  <li>${ele}</li>
-                ` 
-            document.querySelector('.category').appendChild(listItem)
-            })
-          
+          const categories = [];
+          let setCategory;
+     data.forEach(obj => { 
+         categories.push(obj.category)
+     })
+          setCategory = [...new Set(categories)]
+          setCategory.forEach(elem => {
+             let listItem = document.createElement('li')
+            //  listItem.textContent = elem <--> Another way to display the list items
+             listItem.innerHTML =  `
+             <li>${elem}</li>
+             `
+             console.log(listItem);
+             document.querySelector('.category').appendChild(listItem)
+         })
           
     // Getting the Products
           data.forEach(item => {
-              console.log(item);
+              
               const articles = document.createElement('article')
               articles.setAttribute('class', 'article');
               articles.innerHTML = `
